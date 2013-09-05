e1f0e8f0c94172a4fcc6a4b2d326bab907f9eeeb
Pratik Gangwani <Pratik@lawn-128-61-29-32.lawn.gatech.edu>
2013-09-05 19:54:16 -0400

    Initial Commit

---
+++ PascalCalculator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PascalCalculator
+//
+//  Created by Pratik Gangwani on 9/5/13.
+//  Copyright (c) 2013 pgangwani3. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
